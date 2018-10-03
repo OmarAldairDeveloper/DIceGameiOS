e0601e377650c31de5ffebbdc56191ccf8524381
Omar Aldair <omaraldair.developer@gmail.com>
2018-10-02 19:49:45 -0500

    Hemos cambiado la estructura para resumir toda la logica en una sola funcion

--- DiceGame/ViewController.swift
+++ DiceGame/ViewController.swift
@@ -9,9 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    
-    
+
     @IBOutlet weak var diceLeftPlayer: UIImageView!
     @IBOutlet weak var diceRightPlayer: UIImageView!
     @IBOutlet weak var diceLeftIA: UIImageView!
@@ -22,10 +20,12 @@ class ViewController: UIViewController {
     let diceImagesNumber = [1,2,3,4,5,6]
     let nFaces : UInt32
     
+    
     var diceLeftPlayerIndex = 0
     var diceRightPlayerIndex = 0
     var diceLeftIAIndex = 0
     var diceRightIAIndex = 0
+    
     
     var diceLeftPlayerNumber = 0
     var diceRightPlayerNumber = 0
@@ -46,19 +46,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        generateMyRandomDices()
-        generateIARandomDices()
+        generateRandomDices()
     }
     
     
-    func generateMyRandomDices(){
+    func generateRandomDices(){
         
         self.diceLeftPlayerIndex = Int(arc4random_uniform(nFaces))
         self.diceRightPlayerIndex = Int(arc4random_uniform(nFaces))
+        self.diceLeftIAIndex = Int(arc4random_uniform(nFaces))
+        self.diceRightIAIndex = Int(arc4random_uniform(nFaces))
         
         
         UIView.animate(withDuration: 0.3, delay: 0, options: UIView.AnimationOptions.curveEaseInOut, animations: {
             
+            // Dado del jugador
             self.diceLeftPlayer.transform = CGAffineTransform(scaleX: 0.6, y: 0.6).concatenating(CGAffineTransform(rotationAngle: CGFloat.pi/2)).concatenating(CGAffineTransform(translationX: -100, y: 100))
             
             self.diceRightPlayer.transform = CGAffineTransform(scaleX: 0.6, y: 0.6).concatenating(CGAffineTransform(rotationAngle: CGFloat.pi/2)).concatenating(CGAffineTransform(translationX: 100, y: 100))
@@ -67,33 +69,7 @@ class ViewController: UIViewController {
             self.diceRightPlayer.alpha = 0.0
             
             
-        }) { (completed) in
-            
-            self.diceLeftPlayer.transform = CGAffineTransform.identity
-            self.diceRightPlayer.transform = CGAffineTransform.identity
-            
-            self.diceLeftPlayer.alpha = 1.0
-            self.diceRightPlayer.alpha = 1.0
-            
-            self.diceLeftPlayer.image = UIImage(named: self.diceImages[self.diceLeftPlayerIndex])
-            self.diceRightPlayer.image = UIImage(named: self.diceImages[self.diceRightPlayerIndex])
-            
-            
-            self.diceLeftPlayerNumber = Int(self.diceImagesNumber[self.diceLeftPlayerIndex])
-            self.diceRightPlayerNumber = Int(self.diceImagesNumber[self.diceRightPlayerIndex])
-            self.resultSumPlayer = self.diceLeftPlayerNumber + self.diceRightPlayerNumber
-            print("result: \(self.resultSumPlayer)")
-        }
-        
-    }
-    
-    func generateIARandomDices(){
-        
-        self.diceLeftIAIndex = Int(arc4random_uniform(nFaces))
-        self.diceRightIAIndex = Int(arc4random_uniform(nFaces))
-        
-        UIView.animate(withDuration: 0.3, delay: 0, options: UIView.AnimationOptions.curveEaseInOut, animations: {
-            
+            // Dado de la IA
             self.diceLeftIA.transform = CGAffineTransform(scaleX: 0.6, y: 0.6).concatenating(CGAffineTransform(rotationAngle: CGFloat.pi/2)).concatenating(CGAffineTransform(translationX: -100, y: 100))
             
             self.diceRightIA.transform = CGAffineTransform(scaleX: 0.6, y: 0.6).concatenating(CGAffineTransform(rotationAngle: CGFloat.pi/2)).concatenating(CGAffineTransform(translationX: 100, y: 100))
@@ -104,6 +80,23 @@ class ViewController: UIViewController {
             
         }) { (completed) in
             
+            // Dado del jugador
+            self.diceLeftPlayer.transform = CGAffineTransform.identity
+            self.diceRightPlayer.transform = CGAffineTransform.identity
+            
+            self.diceLeftPlayer.alpha = 1.0
+            self.diceRightPlayer.alpha = 1.0
+            
+            self.diceLeftPlayer.image = UIImage(named: self.diceImages[self.diceLeftPlayerIndex])
+            self.diceRightPlayer.image = UIImage(named: self.diceImages[self.diceRightPlayerIndex])
+            
+            
+            self.diceLeftPlayerNumber = self.diceImagesNumber[self.diceLeftPlayerIndex]
+            self.diceRightPlayerNumber = self.diceImagesNumber[self.diceRightPlayerIndex]
+            self.resultSumPlayer = self.diceLeftPlayerNumber + self.diceRightPlayerNumber
+            
+            
+            // Dado de la IA
             self.diceLeftIA.transform = CGAffineTransform.identity
             self.diceRightIA.transform = CGAffineTransform.identity
             
@@ -112,13 +105,40 @@ class ViewController: UIViewController {
             
             self.diceLeftIA.image = UIImage(named: self.diceImages[self.diceLeftIAIndex])
             self.diceRightIA.image = UIImage(named: self.diceImages[self.diceRightIAIndex])
+            
+            
+            self.diceLeftIANumber = self.diceImagesNumber[self.diceLeftIAIndex]
+            self.diceRightIANumber = self.diceImagesNumber[self.diceRightIAIndex]
+            self.resultSumIA = self.diceLeftIANumber + self.diceRightIANumber
+            
+            
+            // Resultado
+            self.checkWiner()
+        
+
+        }
+        
+    }
+    
+
+    func checkWiner(){
+        
+        if self.resultSumPlayer > self.resultSumIA{
+            print("Has ganado. Tu resultado fué: \(self.resultSumPlayer) y el del rival fué: \(self.resultSumIA)")
+        }
+        
+        if self.resultSumPlayer == self.resultSumIA{
+            print("Has empatado. Tu resultado fué \(self.resultSumPlayer) y el del rival fué \(self.resultSumIA)")
+        }
+        
+        if self.resultSumPlayer < self.resultSumIA{
+            print("Has perdido. Tu resultado fué \(self.resultSumPlayer) y el del rival fué \(self.resultSumIA)")
         }
     }
 
+    
     @IBAction func playButton(_ sender: UIButton) {
-        
-        generateMyRandomDices()
-        generateIARandomDices()
+        generateRandomDices()
     }
     
 }
